956cc0a2f79bf11d54ec853187b63203c307938c
Aaron Goshine <greenlig@gmail.com>
2019-11-13 14:49:10 +0000

    fix(): Event bubbling

--- app/js/main.js
+++ app/js/main.js
@@ -1,65 +1,75 @@
-/* global $ */
+/* global  */
 
 var lastDraggedItem;
 
 var streamConfig = [
-  { name: 'Payment service', 'initial': 'PS' },
-  { name: 'Payment Gateway (paypal)', 'initial': 'PG' },
-  { name: 'Payment Gateway (card)', 'initial': 'PG' },
-  { name: 'Business as usual', 'initial': 'BAU' }
+  { 'name': 'Payment service', 'initial': 'PS' },
+  { 'name': 'Payment Gateway (paypal)', 'initial': 'PG' },
+  { 'name': 'Payment Gateway (card)', 'initial': 'PG' },
+  { 'name': 'Business as usual', 'initial': 'BAU' }
 ];
 
 var usersConfig = [
   {
     'name': 'Aaron Goshine',
     'nickname': 'Grand master G',
-    'initial': 'AG'
+    'initial': 'AG',
+    'id': 'id-01'
   },
   {
     'name': 'Hemanth Narravula',
     'nickname': 'Heman',
-    'initial': 'HN'
+    'initial': 'HN',
+    'id': 'id02'
   },
   {
     'name': 'Udaya Lekkala',
     'nickname': 'day day',
-    'initial': 'UL'
+    'initial': 'UL',
+    'id': 'id03'
+
   },
   {
     'name': 'Prasanjit Mohanty',
     'nickname': 'Jit',
-    'initial': 'PM'
+    'initial': 'PM',
+    'id': 'id04'
+
   },
   {
     'name': 'Chris Gordon',
     'nickname': 'young wizzard',
-    'initial': 'CG'
+    'initial': 'CG',
+    'id': 'id05'
+
   },
   {
     'name': 'Chathura Fernando ',
     'nickname': 'Nandos',
-    'initial': 'CH'
+    'initial': 'CH',
+    'id': 'id06'
+
   }
 ];
 
-function allowDrop (event) {
-  event.preventDefault();
-}
-
 function drag (event) {
+  event.preventDefault();
   lastDraggedItem = document.getElementById(event.target.id);
 }
 
-function drop (event, next) {
-  event.target.appendChild(lastDraggedItem);
+function drop (event) {
+  event.preventDefault();
+  var pattern = new RegExp('stream-unit', 'g');
+  if (event.target.className.match(pattern)) {
+    event.target.appendChild(lastDraggedItem);
+  }
   lastDraggedItem = null;
 }
 
-$(function () {
-  // template
-
-  // Instantiate the table with the existing HTML tbody
-  // and the row with the template
+function allowDrop (ev) {
+  ev.preventDefault();
+}
+window.onload = function () {
   var personTpl = document.querySelector('#person');
   var streamInfoTpl = document.querySelector('#stream-info');
   var streamBoxTpl = document.querySelector('#stream-boxes');
@@ -80,15 +90,20 @@ $(function () {
 
   usersConfig.forEach(function (user) {
     var personContent = document.importNode(personTpl.content, true);
+    personContent.querySelectorAll('.person')[0].setAttribute('id', user.id);
     personContent.querySelectorAll('h4')[0].textContent = user.initial;
     var streamList = document.querySelector('#day1 > div:nth-child(1)');
     streamList.appendChild(personContent);
   });
 
-  $('#steams-list');
+  document.querySelectorAll('.person')
+    .forEach(function (item) {
+      item.addEventListener('drag', drag);
+    });
 
-  $('.person').on('drag', drag);
-
-  $('.stream-unit').on('drop', drop);
-  $('.stream-unit').on('dragover', allowDrop);
-});
+  document.querySelectorAll('.stream-unit')
+    .forEach(function (item) {
+      item.addEventListener('drop', drop);
+      item.addEventListener('dragover', allowDrop);
+    });
+};
